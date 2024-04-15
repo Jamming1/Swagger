0f4700f27792783fe04d6971de460b3be9b1ca76
Jamming <j.lucreziaparrillo@gmail.com>
2024-04-15 14:54:56 +0200

    Initial commit

---
+++ src/main/java/com/example/Swagger/NameController.java
@@ -0,0 +1,39 @@
+package com.example.Swagger;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class NameController {
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "name", content =
+                    {
+                            @Content(mediaType = "application/string")
+                    })
+    })
+
+    @Operation(summary = "method name")
+    @GetMapping(path = "/getName")
+    public String name(@Parameter(description = "The user request param") @RequestParam(value = "nome", required = true) String nome) {
+        return nome;
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = " reverse name", content =
+                    {
+                            @Content(mediaType = "application/string")
+                    })
+    })
+
+
+    @Operation(summary = "reverse name")
+    @PostMapping(path = "/getReverseName")
+    public String reverseName(@Parameter(description = "reverse name param") @RequestParam(value = "name", required = true) String name) {
+        return new StringBuilder(name).reverse().toString();
+    }
+}
